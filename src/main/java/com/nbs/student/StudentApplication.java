da50bdc6b1655ee5313dc1b3ce098a29f098ec47
Ahmed <ahmedh@procuredox.com>
2025-02-09 17:58:00 +0200

    adding docker file

--- src/main/java/com/nbs/student/StudentApplication.java
+++ src/main/java/com/nbs/student/StudentApplication.java
@@ -9,28 +9,10 @@ import java.net.URI;
 
 
 @SpringBootApplication
-public class StudentApplication implements CommandLineRunner {
+public class StudentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) {
-		String swaggerUrl = "http://localhost:8080/api/swagger-ui/index.html";
-		openSwaggerUiInBrowser(swaggerUrl);
-	}
-
-	private void openSwaggerUiInBrowser(String url) {
-		try {
-			if (Desktop.isDesktopSupported()) {
-				Desktop.getDesktop().browse(new URI(url));
-			} else {
-				// Fallback for Linux headless environments
-				Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 }
